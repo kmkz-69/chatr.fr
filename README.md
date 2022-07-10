564429479fc642cb88cd308b7f6de720643984e9
Jason Michel YAWILHIT <jayson77190@gmail.com>
2022-07-10 09:12:12 +0200

    update

--- README.md
+++ README.md
@@ -1,19 +1,8 @@
-![Chatr](https://raw.githubusercontent.com/kmkz-69/vscode-snippet-copilote/main/icon.png)
-# chatr
-is a video calling app bundle with Peer to Peer , screen sharing and whiteboard collaboration.
-You can deploy web (PWA), Android, Mac, Windows, and Linux binaries from this package
-
-![Chatr](https://github.com/kmkz-69/chatr.fr/blob/main/ScreenShot.png)
-The frontend app is developed on the Ionic framework with React and the backend API is developed on Express.js.
-Golang ,Rust Backend coming soon.
-We using the sqlite mysql or postgress database. And we using WebRTC protocol for video calling and Websockets protocol for real-time communication
-
 # Use Case
 * Live events
 * Real estate tour
 * Meetup
 
-```
 npx cap add electron
 npx cap copy
 npx cap open electron
@@ -22,17 +11,3 @@ npx cap add android
 npx cap sync
 npx cap copy
 npx cap open android
-```
- You can ask questions and initiate [discussions](https://github.com/kmkz-69/chatr.fr/discussions) about Chatr topics in the `chatr` repository on GitHub.
-
-👉 [**Ask a question**](https://github.com/kmkz-69/chatr.fr/discussions/new)
-
-<h3 align="left">Support:</h3>
-<p><a href="https://ko-fi.com/chatr"> 
-  <img align="left" src="https://cdn.ko-fi.com/cdn/kofi3.png?v=3" height="50" width="210" alt="chatr" />
-  </a>
-</p>
-<br>
-
-👉 [**BACKEND NODEJS **](https://github.com/kmkz-69/Chatr-Backend.git) <br>
-👉 This is still in dev mod  [**BACKEND Golang **](https://github.com/kmkz-69/GO-chatr-app-backend.git)
--- src/services/PeerService.ts
+++ src/services/PeerService.ts
@@ -1,102 +1,101 @@
-import Peer, { MediaConnection } from 'peerjs';
-import { History } from 'history';
-import { includes } from 'lodash';
-import routes from '../routes';
-import AlertService from './AlertService';
-import 'dotenv/config';
+import Peer, { MediaConnection } from 'peerjs'
+import { History } from 'history'
+import { includes } from 'lodash'
+import routes from '../routes'
+import AlertService from './AlertService'
 
 export default class PeerService {
   private static options = {
     host: 'localhost',
-    port: 3001,
+    port: 3002,
     path: '/peer',
     secure: true,
-  };
-
-  private client: Peer;
-
-  constructor() {
-    this.client = new Peer(PeerService.options);
+    debug : 3
   }
 
-  private getErrorMessage(type: string): string {
+  private client: Peer
+
+  constructor () {
+    this.client = new Peer(PeerService.options)
+  }
+
+  private getErrorMessage (type: string): string {
     switch (type) {
       case 'browser-incompatible':
-        return "Device doesn't support WebRTC";
+        return 'Device doesn\'t support WebRTC'
       case 'disconnected':
-        return 'Already disconnected meeting';
+        return 'Already disconnected meeting'
       case 'invalid-id':
-        return 'Invalid connection ID';
+        return 'Invalid connection ID'
       case 'invalid-key':
-        return 'Invalid API key';
+        return 'Invalid API key'
       case 'network':
-        return "Can't establish network connection";
+        return 'Can\'t establish network connection'
       case 'peer-unavailable':
-        return "Meeting ID doesn't exist";
+        return 'Meeting ID doesn\'t exist'
       case 'ssl-unavailable':
-        return "Server doesn't support SSL";
+        return 'Server doesn\'t support SSL'
       case 'server-error':
-        return 'Unable to reach server';
+        return 'Unable to reach server'
       case 'socket-error':
-        return 'Underlying socket error';
+        return 'Underlying socket error'
       case 'socket-closed':
-        return 'Underlying socket closed';
+        return 'Underlying socket closed'
       case 'unavailable-id':
-        return 'Connection ID already taken';
+        return 'Connection ID already taken'
       case 'webrtc':
-        return 'WebRTC error';
+        return 'Device doesn\'t support WebRTC'
       default:
-        return 'Something went wrong';
+        return 'Something went wrong'
     }
   }
 
-  handleErrors(history: History) {
-    this.client.on('error', (error) => {
-      const { type } = error;
-      const message = this.getErrorMessage(type);
-      AlertService.push(message);
-      const routeErrorTypes = ['network', 'server-error', 'peer-unavailable'];
-      const canRoute = includes(routeErrorTypes, type);
-      if (canRoute) history.push(routes.home.path);
-    });
+  handleErrors (history: History) {
+    this.client.on('error', error => {
+      const { type } = error as any
+      const message = this.getErrorMessage(type)
+      AlertService.push(message)
+      const routeErrorTypes = ['network', 'server-error', 'peer-unavailable']
+      const canRoute = includes(routeErrorTypes, type)
+      if (canRoute) history.push(routes.home.path)
+    })
   }
 
-  onOpen(callback: (localConnectionId: string) => void): void {
-    this.client.on('open', (localConnectionId) => callback(localConnectionId));
+  onOpen (callback: (localConnectionId: string) => void): void {
+    this.client.on('open', localConnectionId => callback(localConnectionId))
   }
 
-  call(
+  call (
     meetingId: string,
     stream: MediaStream,
-    callback: (call: MediaConnection) => void,
-  ): void {
-    const call = this.client.call(meetingId, stream);
-    callback(call);
+    callback: (call: MediaConnection) => void): void {
+    const call = this.client.call(meetingId, stream)
+    callback(call)
   }
 
-  onReceiveCall(callback: (call: MediaConnection) => void): void {
-    this.client.on('call', (call) => callback(call));
+  onReceiveCall (callback: (call: MediaConnection) => void): void {
+    this.client.on('call', call => callback(call))
   }
 
-  answerCall(call: MediaConnection, localStream: MediaStream): void {
-    call.answer(localStream);
+  answerCall (call: MediaConnection, localStream: MediaStream): void {
+    call.answer(localStream)
   }
 
-  onCalling(
+  onCalling (
     call: MediaConnection,
-    callback: (remoteConnectionId: string, remoteStream: MediaStream) => void,
-  ): void {
-    call.on('stream', (remoteStream) => {
-      const remoteConnectionId = call.peer;
-      callback(remoteConnectionId, remoteStream);
-    });
+    callback: (remoteConnectionId: string,
+    remoteStream: MediaStream) => void): void {
+    call.on('stream', remoteStream => {
+      const remoteConnectionId = call.peer
+      callback(remoteConnectionId, remoteStream)
+    })
   }
 
-  disconnect(): void {
-    this.client.destroy();
+  disconnect (): void {
+    this.client.destroy()
   }
 
-  onDisconnect(callback: () => void): void {
-    this.client.on('close', () => callback());
+  onDisconnect (callback: () => void): void {
+    this.client.on('close', () => callback())
   }
 }
